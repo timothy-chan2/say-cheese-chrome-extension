f8eb0a1b24b206ff0a9957aabb405c1b10adfe75
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-10-31 19:19:16 -0400

    Add a randomly generated number at the end of the URL so it does not show the same image everywhere

--- scripts/cameraify.js
+++ scripts/cameraify.js
@@ -1,6 +1,6 @@
 const cameraify = () => {
   document.querySelectorAll('img').forEach( (img) => {
-    img.src = `https://source.unsplash.com/featured/?camera/${img.width}x${img.height}`;
+    img.src = `https://source.unsplash.com/featured/?camera/${img.width}x${img.height}?${Math.random()}`;
     img.srcset = img.src;
   });
 }
